2a8734b47b359c1eedc7c280eb19ade3325772a7
Titus-Lai <88089922+Titus-Lai@users.noreply.github.com>
2023-05-24 12:52:49 +1000

    Update app.py

--- Admin 101/app.py
+++ Admin 101/app.py
@@ -2,12 +2,12 @@ from flask import Flask, render_template, request, url_for, redirect, session
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 #set app as a Flask instance 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='',  static_folder='static')
 #encryption relies on secret keys so they could be run
 app.secret_key = "testing"
 
 def db_connect(): 
-    client = MongoClient("mongodb+srv://username:8DGG3qnguYUdOClX@cluster0.w6o27sx.mongodb.net/?retryWrites=true&w=majority", server_api=ServerApi('1'))
+    client = MongoClient("mongodb+srv://717u5:wMz8xEJib5wUZIo5@cluster0.w6o27sx.mongodb.net/?retryWrites=true&w=majority" , server_api=ServerApi('1'))
     return client
 
 # Create admin user
@@ -50,9 +50,14 @@ def find_user(email, password):
 
     return out
 
-#assign URLs to have a particular route 
-@app.route("/", methods=['post', 'get'])
+#Starting point
+@app.route("/")
 def index():
+    return render_template('start.html')
+
+#Register page
+@app.route("/register.html", methods=['post', 'get'])
+def register():
     message = ''
     #if method post in index
     if "email" in session:
@@ -67,13 +72,13 @@ def index():
         email_found = find_user_email(email)
         if user_found:
             message = 'There already is a user by that name'
-            return render_template('index.html', message=message)
+            return render_template('register.html', message=message)
         if email_found:
             message = 'This email already exists in database'
-            return render_template('index.html', message=message)
+            return render_template('register.html', message=message)
         if password1 != password2:
             message = 'Passwords should match!'
-            return render_template('index.html', message=message)
+            return render_template('register.html', message=message)
         else:
             #insert it in the record collection
             insert_user(user, email, password2)
@@ -83,8 +88,9 @@ def index():
             new_email = user_data['email']
             #if registered redirect to logged in as the registered user
             return render_template('logged_in.html', email=new_email)
-    return render_template('index.html')
+    return render_template('register.html')
 
+#Login page
 @app.route("/login.html", methods=["POST", "GET"])
 def login():
     message = 'Please login to your account'
@@ -98,6 +104,10 @@ def login():
         #check if email exists in database
         user = find_user(email, password)
         if user:
+            session["email"] = "admin333@yahoo.com"
+            return redirect(url_for('admin'))
+            
+        if user:
             session["email"] = email
             return redirect(url_for('logged_in'))
         else:
@@ -105,11 +115,21 @@ def login():
             return render_template('login.html', message=message)
     return render_template('login.html', message=message)
 
+#Logged_in page
 @app.route('/logged_in.html')
 def logged_in():
     if "email" in session:
         email = session["email"]
         return render_template('logged_in.html', email=email)
+    else:
+        return redirect(url_for("login"))
+
+#Admin page
+@app.route('/admin.html')
+def admin():
+    if "email" in session:
+        email = session["email"]
+        return render_template('admin.html', email=email)
     else:
         return redirect(url_for("login"))
 
@@ -119,7 +139,7 @@ def signout():
         session.pop("email", None)
         return render_template("signout.html")
     else:
-        return render_template('index.html')
+        return render_template('register.html')
 
 if __name__ == "__main__":
     print("Initialising DB")
